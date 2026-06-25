d79e2f3337bdb06d71bbc2b858647f420d8ca596
415-31-krissanakantinan <56858@yupparaj.ac.th>
2026-06-25 09:01:08 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+st.title("My Digital Profile")
+
+st.heade("สวัสดีครับ")
+st.write("นายกฤษณกันตินันท์ ณ นคร")
+st.write("ชื่อเล่นโนอา")
+st.write("เลขที่ 31 ม.4/15")
+
+st.header("เป้าหมายและสิ่งที่อยากทําในอนาคต")
+st.write("เป้าหมาย: อยากไปเรียนต่างประเทศ")
+st.write("สิ่งที่อยากเป็นในอนาคต: Cafe owner")
