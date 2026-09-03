23b82e83a7b4864b1320189eeda9eec37ee8b955
415-31-krissanakantinan <56858@yupparaj.ac.th>
2026-09-03 08:45:19 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,6 +101,6 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นายกฤษณกันตินันท์ ณ นคร ม.4/15")
 
 
